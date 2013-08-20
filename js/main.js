49cb6dfce7140a1be2dc2e8f97a3d898feb456b9
bigfree <okukster@gmail.com>
2013-08-20 13:51:11 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,42 +1,23 @@
 ;(function($) {
 	$(document).ready(function () {
 		
-		/*
-		 * Cufon
-		 * @font-family: Nazov pisma
-		 * @hover: true, false
-		 */
-		//	Cufon.replace('.cufon', {fontFamily: '', hover: true});
-		
-		;(function() {
-			/*
-			 * Anonymous function self calling
-			 */
-			 
-			 /*
-			  * Vyvolanie fancyboxu
-			  */
-			$(".fancybox").fancybox();
-
-			$("#kontroluj").checkForm({
-				ajax: true,
-				debug: false,
-				init: function( opt ) {
-					console.log("INIT: Plugin sa spustil");
-					// Vlastny option
-					// opt['vlastnyOption'] = "Skuska";
-					// console.log(opt);
-				},
-				error: function( numberOfInvalid, IDItemJSON ) {
-					console.log('Pocet chybnych elementov: ' + numberOfInvalid);
-					console.log(IDItemJSON);
-				},
-				success: function( value ) {
-					console.log(value);
-				}
-			});
-
-		})();
+		$("#kontroluj").checkForm({
+			ajax: true,
+			debug: true,
+			init: function( opt ) {
+				console.log("INIT: Plugin sa spustil");
+				// Vlastny option
+				// opt['vlastnyOption'] = "Skuska";
+				// console.log(opt);
+			},
+			error: function( numberOfInvalid, IDItemJSON ) {
+				console.log('Pocet chybnych elementov: ' + numberOfInvalid);
+				console.log(IDItemJSON);
+			},
+			success: function( value ) {
+				console.log(value);
+			}
+		});
 
 	});
 })(jQuery);
